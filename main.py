69da5528e339111313b1c620106b247411d019f6
Florian Vierkant <florian@vierkant-net.de>
2024-04-15 15:16:38 +0200

    Test Push

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def invert_colors(input_image):
 
 def sepia_filter(input_image):
     filter = np.array([
-        [0.393, 0.769, 0.189],
+        [0.393, 0.769, 0.180],
         [0.349, 0.686, 0.168],
         [0.272, 0.534, 0.131]
     ])
