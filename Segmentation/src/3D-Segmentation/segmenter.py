5ab36c8cf9c5e23598e2c294c7a55642da58cf1d
dennis.papenfuss <dennispapenfuss@student.uni-luebeck.de>
2024-06-17 19:13:14 +0200

    Add argument parser (unused)
    Former-commit-id: 425fa01f9f99d47e5015ea0edb3a603baa4d7f0c
    Former-commit-id: b185379fc028cbee40326244482846b9b0eaeebe

--- Segmentation/src/3D-Segmentation/segmenter.py
+++ Segmentation/src/3D-Segmentation/segmenter.py
@@ -1,9 +1,13 @@
 import torch
+from model import UNet
+import torch
+import pytorch_lightning as pl
 
 
 class Segmenter(pl.LightningModule):
     def __init__(self):
         super().__init__()
+        self.save_hyperparameters()
         self.model = UNet()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-4)
         self.loss_fn = torch.nn.CrossEntropyLoss()
@@ -14,27 +18,27 @@ class Segmenter(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         # This is the CT scan. In torchio, the batch objects doesn't only contain the core volume data, but also
         # some metadata. Hence, we need to extract its data attribute.
-        img = batch["CT"]["data"]
+        img = batch["MRI"]["data"]
         # In order to use cross-entropy loss, we need to remove the channel dimension. TODO Why??
         mask = batch["Label"]["data"][:, 0]
         mask = mask.long()
         
         pred = self(img)
         loss = self.loss_fn(pred, mask)
-        self.log("Train Loss", loss)
+        self.log("Train loss", loss)
         return loss
     
     def validation_step(self, batch, batch_idx):
         # This is the CT scan. In torchio, the batch objects doesn't only contain the core volume data, but also
         # some metadata. Hence, we need to extract its data attribute.
-        img = batch["CT"]["data"]
+        img = batch["MRI"]["data"]
         # In order to use cross-entropy loss, we need to remove the channel dimension. TODO Why??
         mask = batch["Label"]["data"][:, 0]
         mask = mask.long()
         
         pred = self(img)
         loss = self.loss_fn(pred, mask)
-        self.log("Val Loss", loss)
+        self.log("Val loss", loss)
         return loss
     
     def configure_optimizers(self):
--- Segmentation/src/3D-Segmentation/train.py
+++ Segmentation/src/3D-Segmentation/train.py
@@ -1,3 +1,4 @@
+from argparse import Namespace, ArgumentParser
 from pathlib import Path
 
 import torchio as tio
@@ -9,6 +10,7 @@ from pytorch_lightning.loggers import TensorBoardLogger
 from model import UNet
 from segmenter import Segmenter
 
+
 # TODO Change this function appropriately
 def change_img_to_label_path(path):
     """ Returns all directories in a path. """
@@ -17,50 +19,98 @@ def change_img_to_label_path(path):
     parts[parts.index("imagesTr")] = "labelsTr"
     return Path(*parts)
 
+def get_cmd_args() -> Namespace:
+
+    parser = ArgumentParser()
+    parser.add_argument("--train-path", dest="train_path", default="Preprocessed/train",
+                        help="The directory where the training data is located.")
+    parser.add_argument("--val-path", dest="val_path", default="Preprocessed/val",
+                        help="The directory where the validation data is located.")
+    parser.add_argument("--device", dest="device", default="auto",
+                        choices=["auto", "cpu", "gpu", "cuda", "mps", "tpu"],
+                        help="Which device to use, e.g., 'cpu' or 'gpu'. "
+                             "Full list: https://lightning.ai/docs/pytorch/stable/extensions/accelerator.html")
+    parser.add_argument("--batch-size", dest="batch_size", default="4",
+                        help="The number of elements to train with per batch (with or without quotation marks).")
+    parser.add_argument("--epochs", dest="num_epochs", default="30",
+                        help="The number of epochs to train for (with or without quotation marks).")
+    parser.add_argument("--output-dir", dest="output_dir", default="logs",
+                        help="The output directory for the model checkpoints.")
+    parser.add_argument("--kernel-size", dest="kernel_size", default="3",
+                        help="The kernel size to use in the double-convolutions of the UNet. Must be odd.")
+    parser.add_argument("--activation-fn", dest="activation_fn", default="relu",
+                        help="The activation function to use in the double-convolutions of the UNet. Must be "
+                             "one of those listed here: https://pytorch.org/docs/stable/nn.html#non-linear"
+                             "-activations-weighted-sum-nonlinearity,"
+                             "in lowercase. For example, 'relu' corresponds to torch.nn.ReLU() and 'logsoftmax'"
+                             "corresponds to torch.nn.LogSoftmax().")
+    args = parser.parse_args()
+
+    return args
+
 
 def main():
+    # Fetch CMD arguments, including hyperparameters like the batch size.
+    cmd_args: Namespace = get_cmd_args()
+
+    # CMD parameters
+    train_path = Path(cmd_args.train_path)
+    val_path = Path(cmd_args.val_path)
+    device = cmd_args.device
+
+    # Hyperparameters
+    batch_size = int(cmd_args.batch_size)
+    num_epochs = int(cmd_args.num_epochs)
+    kernel_size = int(cmd_args.kernel_size)
+
     # ----------- Preprocessing
 
     num_train_elements = 400
 
-    path = Path("Task03_Liver_rs/imagesTr/")
-    subject_paths = list(path.glob("liver_*"))
+    # Read in the raw data, not the preprocessed data. The preprocessing takes place in this file.
+    path = Path("D:/Deep Learning/Task01_BrainTumour/imagesTr")
+    subject_paths = list(path.glob("BRATS_*"))
     subjects = []
 
+    # The subjects are tio objects containing the MRI scan and the label.
     for subject_path in subject_paths:
         label_path = change_img_to_label_path(subject_path)
-        subject = tio.Subject({"CT": tio.ScalarImage(subject_path), 
-                            "Label": tio.LabelMap(label_path)})
+        subject = tio.Subject({"MRI": tio.ScalarImage(subject_path),
+                               "Label": tio.LabelMap(label_path)})
         subjects.append(subject)
 
     for subject in subjects:
-        assert subject["CT"].orientation == ("R", "A", "S")
+        assert subject["MRI"].orientation == ("R", "A", "S")
 
+    # The first step shouldn't change anything for the given brain tumor dataset, since the scans are all the
+    # same size anyway. The second step replaces the normalization step of the 2D segmentation.
     process = tio.Compose([
-        tio.CropOrPad((256, 256, 200)),
+        tio.CropOrPad((240, 240, 155)),
         tio.RescaleIntensity((-1, 1))
     ])
 
-    # The augmentation creates new imaages with scales between 0.9 and 1.1 as well as rotating by between -10 degrees and 10
-    # degrees.
-    augmentation = tio.RandomAffine(scales=(0.9, 1.1), degrees=(-10, 10))
+    # The augmentation creates new images with scales between 0.9 and 1.1 as well as rotating by between -10 degrees
+    # and 10 degrees.
+    augmentation = tio.RandomAffine(scales=(0.9, 1.1), degrees=(-25, 25))
 
-    # The validatoin only gets the preprocessed data, whereas we create new images for the test data in the form of
+    # The validation only gets the preprocessed data, whereas we create new images for the test data in the form of
     # augmented data with the above transformations.
     val_transform = process
     train_transform = tio.Compose([process, augmentation])
 
     # This is the train/test split:
-    print(f"Training: {num_train_elements / len(subject_paths):.4f}, val: {1 - num_train_elements / len(subject_paths):.4f}")
-
+    print(f"Training: {num_train_elements / len(subject_paths):.4f}, "
+          f"val: {1 - num_train_elements / len(subject_paths):.4f}")
 
     train_dataset = tio.SubjectsDataset(subjects[:num_train_elements], transform=train_transform)
     val_dataset = tio.SubjectsDataset(subjects[num_train_elements:], transform=val_transform)
 
     # The sampler decides with what probability each label should occur in the training. This allows us to oversample
     # the tumors by assigning the tumor label a higher probability. Specifically, background (label 0) has probability
-    # 0.2, liver (label 1) has probability 0.3, and tumor (label 2) has probability 0.5.
-    sampler = tio.data.LabelSampler(patch_size=96, label_name="Label", label_probabilities={0:0.2, 1:0.3, 2:0.5})
+    # 0.2, liver (label 1) has probability 0.3, and tumor (label 2) has probability 0.5. The probabilities refer
+    # to how likely it is that the corresponding label is in the middle of the patch.
+    sampler = tio.data.LabelSampler(patch_size=96, label_name="Label",
+                                    label_probabilities={0: 0.4, 1: 0.3, 2: 0.15, 3: 0.15})
 
     # This is the queue that generates the actual patches from the images. The values max_length and num_workers
     # can be very memory-consuming and therefore have to be adapted to the specific hardware you're running this code
@@ -81,22 +131,28 @@ def main():
         num_workers=4
     )
 
-
-
     # ----------- Data loading and training
-    train_loader = torch.utils.data.DataLoader(train_patches_queue, batch_size=2, num_workers=0)
-    val_loader = torch.utils.data.DataLoader(val_patches_queue, batch_size=2, num_workers=0)
+    train_loader = torch.utils.data.DataLoader(train_patches_queue, batch_size=4, num_workers=0,
+                                               pin_memory=True)
+    val_loader = torch.utils.data.DataLoader(val_patches_queue, batch_size=4, num_workers=0,
+                                             pin_memory=True)
 
     model = Segmenter()
     checkpoint_callback = ModelCheckpoint(
-        monitor="Val Loss",
+        monitor="Val loss",
         save_top_k=10,
         mode="min"
     )
 
-    trainer = pl.Trainer(devices=[0], accelerator="cuda", logger=TensorBoardLogger(save_dir="./logs"), log_every_n_steps=1, 
-                        callbacks=checkpoint_callback, max_epochs=30)
+    print("Our model:")
+    print(model)
+
+    trainer = pl.Trainer(devices=[0], accelerator="cuda",
+                         logger=TensorBoardLogger(save_dir="logs"),
+                         log_every_n_steps=1,
+                         callbacks=[checkpoint_callback], max_epochs=10)
     trainer.fit(model, train_loader, val_loader)
+
 
 if __name__ == "__main__":
     main()
