f347a7483ef95267b7116abd56bec928cca309be
Florian Vierkant <florian@vierkant-net.de>
2024-06-18 15:57:08 +0200

    Add Backend API & containerized nnUnet

---
+++ papaya-svelte/src/components/Loading.svelte
@@ -0,0 +1,17 @@
+<style>
+    .spinner {
+      width: 15px;
+      height: 15px;
+      border-radius: 50%;
+      border: 4px solid #ccc;
+      border-top-color: #3498db;
+      animation: spin 0.8s linear infinite;
+    }
+  
+    @keyframes spin {
+      to { transform: rotate(360deg); }
+    }
+  </style>
+  
+  <div class="spinner"></div>
+  
