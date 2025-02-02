c582bb262a28dd85eca18cd6386bc114843a17f4
sebastian <sebastian.guhl@student.uni-luebeck.de>
2025-02-02 19:31:54 +0100

    remove trailing slashes from sequence names

--- backend/server/main/routes.py
+++ backend/server/main/routes.py
@@ -495,7 +495,7 @@ def create_project():
         # Add all sequences to the database
         for sequence_data in file_infos:
             # remove trailing '/' of sequence_name
-            sequence_name = sequence_data.get('sequence_name').replace("/", "").replace("\\", "")
+            sequence_name = sequence_data.get('sequence_name')
             sequence_type = sequence_data.get('sequence_type')
             size_in_bytes = sequence_data.get('size_in_bytes')
             selected = sequence_data.get('selected')
