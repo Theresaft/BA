f93dcfd55ea009a9357e9a8ccc7c12f50aff8afe
Florian Vierkant <florian@vierkant-net.de>
2025-04-25 20:54:32 +0200

    Style login and registration card

--- frontend/src/single-components/Register.svelte
+++ frontend/src/single-components/Register.svelte
@@ -29,31 +29,75 @@
     }
 </script>
 
-<div>
-    <h1>Account erstellen</h1>
-    <p id="description">
-        Erstellen Sie hier einen neuen Account. Es sind lediglich E-Mail Adressen der Universität zu Lübeck sowie der UKSH zugelassen.
-        Zusätzlich müssen die E-Mail Adressen durch den Administrator freigeschaltet werden. 
-    </p>
+<div class="card-container">
+    <div class="card">
+        <div>
+            <h1 class="header">Account erstellen</h1>
+            <p id="description">
+                Erstellen Sie hier einen neuen Account. Es sind lediglich E-Mail Adressen der Universität zu Lübeck sowie der UKSH zugelassen.
+                Zusätzlich müssen die E-Mail Adressen durch den Administrator freigeschaltet werden. 
+            </p>
+        </div>
+        <form on:submit|preventDefault={handleAccountCreation}>
+            <input type="text" bind:value={user_mail} placeholder="Email" required />
+            <input type="password" bind:value={password} placeholder="Passwort" required />
+            <button type="submit" class="login-button">Account anlegen</button>
+            {#if error}
+                <p class="error">{error}</p>
+            {/if}
+            <!-- Button zum Wechseln zur Account-Erstellung -->
+            <div class="back-arrow" on:click={() => dispatcher('toggleAccountCreation')}>
+                ← Zurück zum Login
+            </div>
+        </form>
+    </div>
 </div>
-<form on:submit|preventDefault={handleAccountCreation}>
-    <input type="text" bind:value={user_mail} placeholder="Email" required />
-    <input type="password" bind:value={password} placeholder="Passwort" required />
-    <button type="submit">Account anlegen</button>
-    {#if error}
-        <p class="error">{error}</p>
-    {/if}
-</form>
-<!-- Button zum Wechseln zur Account-Erstellung -->
-<button on:click={() => dispatcher('toggleAccountCreation')}>Login</button>
+
 
 <style>
+    .back-arrow {
+        cursor: pointer;
+        color: var(--button-text-color-secondary);
+        font-weight: 500;
+        text-align: left;
+        margin-top: 10px;
+        transition: color 0.2s ease;
+    }
+
+    .back-arrow:hover {
+        color: #0056b3;
+        text-decoration: underline;
+    }
+
+    .card-container{
+        margin-top: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .card{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 1.5rem;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        gap: 30px;
+        max-width: 500px;
+    }
+    .header{
+        text-align: center;
+    }
+    #description{
+        text-align: center;
+    }
     form {
         display: flex;
         flex-direction: column;
         gap: 10px;
-        max-width: 300px;
-        margin: auto;
+        width: 100%;
+        max-width: 350px;
     }
 
     input {
@@ -65,13 +109,9 @@
     button {
         font-size: 15px;
         font-weight: 600;
+        margin-top: 20px;
         margin-left: 0;
         margin-right: 0;
-        margin-top: 20px;
-    }
-
-    #description {
-        margin-bottom: 100px;
     }
 
     p.error {
